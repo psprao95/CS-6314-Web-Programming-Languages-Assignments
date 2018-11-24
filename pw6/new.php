60d2f911f12abf515edfc6f46d2692fdf9794a16
Prashanth Rao <psprao95@gmail.com>
2018-11-23 18:19:12 -0600

    changes

--- pw6/new.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('flickr.php');
-$Flickr = new Flickr('78dc2b13d9cc1127139a1aa12bf9fcc6');
-$data = $Flickr->search('funny dogs');
-foreach($data['photos']['photo'] as $photo) {
- // the image URL becomes somthing like
- // http://farm{farm-id}.static.flickr.com/{server-id}/{id}_{secret}.jpg
- echo '<img src="' . 'http://farm' . $photo["farm"] . '.static.flickr.com/' . $photo["server"] . '/' . $photo["id"] . '_' . $photo["secret"] . '.jpg">';
-?>
